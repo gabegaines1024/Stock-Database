c88152fa6d54a77a76be1d987ca4866c01766f3a
gkgaines <gabegaines1024@gmail.com>
2025-11-15 19:10:58 -0600

    very early completion, need to work on execution 500 error on docs

---
+++ app/models/__init__.py
@@ -0,0 +1,4 @@
+from app.models.model import Base, User, Stock, Portfolio, Transaction
+
+__all__ = ["Base", "User", "Stock", "Portfolio", "Transaction"]
+
---
+++ app/schemas/__init__.py
@@ -0,0 +1,46 @@
+from app.schemas.schemas import (
+    # Stock schemas
+    StockBase,
+    StockCreate,
+    StockUpdate,
+    Stock,
+    # Portfolio schemas
+    PortfolioBase,
+    PortfolioCreate,
+    PortfolioUpdate,
+    Portfolio,
+    # Transaction schemas
+    TransactionBase,
+    TransactionCreate,
+    TransactionUpdate,
+    Transaction,
+    # User schemas
+    UserBase,
+    UserCreate,
+    UserUpdate,
+    User,
+)
+
+__all__ = [
+    # Stock schemas
+    "StockBase",
+    "StockCreate",
+    "StockUpdate",
+    "Stock",
+    # Portfolio schemas
+    "PortfolioBase",
+    "PortfolioCreate",
+    "PortfolioUpdate",
+    "Portfolio",
+    # Transaction schemas
+    "TransactionBase",
+    "TransactionCreate",
+    "TransactionUpdate",
+    "Transaction",
+    # User schemas
+    "UserBase",
+    "UserCreate",
+    "UserUpdate",
+    "User",
+]
+
