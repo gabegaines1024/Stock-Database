fc9cc05ff9ea87ede8705c7ea38f5caaf793f096
Gabe <gabegaines1024@gmail.com>
2025-11-20 11:26:20 -0500

    added token schema

--- backend/app/schemas/schemas.py
+++ backend/app/schemas/schemas.py
@@ -117,3 +117,10 @@ class User(UserBase):
     
     class Config:
         from_attributes = True
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
